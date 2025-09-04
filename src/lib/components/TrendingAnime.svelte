2135446695f0328483d5e2cb03545bfb4bb55b72
Kat <78310932+Krekensis@users.noreply.github.com>
2025-09-04 22:56:33 +0530

    Update TrendingAnime.svelte

--- src/lib/components/TrendingAnime.svelte
+++ src/lib/components/TrendingAnime.svelte
@@ -45,7 +45,7 @@
 							</div>
 						</div>
 
-						<div class="flex flex-row items-left flex-wrap gap-1.5 sm:gap-2">
+						<div class="flex flex-row items-left flex-wrap gap-1.5 sm:gap-1.5">
 							<div class="text-[10px] sm:text-sm font-bold font-mono rounded-sm bg-white/5 text-gray-400 group-hover:text-gray-300 px-1 py-0.5  transition-colors duration-300">
 								{anime.format}
 							</div>
